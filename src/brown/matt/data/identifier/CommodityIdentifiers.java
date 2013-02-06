b7179b6df29f7e69f1c2e554192662fe43975c74
unknown <mbrown33@ksu.edu>
2013-02-06 17:11:27 -0600

    Added ability to retrieve data for individual commodities

---
+++ src/brown/matt/data/identifier/CommodityIdentifiers.java
@@ -0,0 +1,34 @@
+package brown.matt.data.identifier;
+
+public class CommodityIdentifiers {
+	
+	public static final String CRUDE_OIL = "Crude Oil";
+	public static final String HEATING_OIL = "Heating Oil";
+	public static final String NATURAL_GAS = "Natural Gas";
+	public static final String RBOB_GAS = "RBOB Gas";
+	
+	public static final String CORN = "Corn";
+	public static final String OATS = "Oats";
+	public static final String SOYBEANS = "Soybeans";
+	public static final String SOYBEAN_MEAL = "Soybean Meal";
+	public static final String SOYBEAN_OIL = "Soybean Oil";
+	public static final String WHEAT = "Wheat";
+	
+	public static final String FEEDER_CATTLE = "Feeder Cattle";
+	public static final String LIVE_CATTLE = "Live Cattle";
+	public static final String LEAN_HOGS = "Lean Hogs";
+	public static final String MILK_CLASS_III = "Milk Class III";
+	
+	public static final String GOLD = "Gold";
+	public static final String COPPER = "Copper";
+	public static final String PLATINUM = "Platinum";
+	public static final String SILVER = "Silver";
+	
+	public static final String COCOA = "Cocoa";
+	public static final String COFFEE = "Coffee";
+	public static final String COTTON = "Cotton";
+	public static final String LUMBER = "Lumber";
+	public static final String ORANGE_JUICE = "Orange Juice";
+	public static final String SUGAR = "Sugar";
+	
+}
--- src/brown/matt/data/quote/CommoditiesDataProvider.java
+++ src/brown/matt/data/quote/CommoditiesDataProvider.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
+import brown.matt.data.identifier.CommodityIdentifiers;
 import brown.matt.data.identifier.EnergyIdentifiers;
 import brown.matt.data.identifier.GrainIdentifiers;
 import brown.matt.data.identifier.MeatIdentifiers;
@@ -21,6 +22,145 @@ public class CommoditiesDataProvider {
 	private static final String BASE_URL = "http://download.finance.yahoo.com/d/quotes.csv?s=";
 	//Symbol, Name, last trade, date of last trade, time of last trade, open, high , low, day's change, Previous Close
 	private static final String TAGS = "&f=snl1d1t1ohgw1p";
+	
+	public static List<CommodityData> getSelectData(List<String> selections) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		boolean first = true;
+		
+		for (String select: selections) {
+			switch (select) {
+				case CommodityIdentifiers.COCOA:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(SoftIdentifiers.getCocoa());	
+					break;
+				case CommodityIdentifiers.COFFEE:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(SoftIdentifiers.getCoffee());	
+					break;
+				case CommodityIdentifiers.COPPER:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(MetalIdentifiers.getCopper());	
+					break;	
+				case CommodityIdentifiers.CORN:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(GrainIdentifiers.getCorn());	
+					break;	
+				case CommodityIdentifiers.COTTON:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(SoftIdentifiers.getCotton());	
+					break;	
+				case CommodityIdentifiers.CRUDE_OIL:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(EnergyIdentifiers.getCrudeOil());	
+					break;
+				case CommodityIdentifiers.FEEDER_CATTLE:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(MeatIdentifiers.getFeederCattle());	
+					break;	
+				case CommodityIdentifiers.GOLD:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(MetalIdentifiers.getGold());	
+					break;	
+				case CommodityIdentifiers.HEATING_OIL:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(EnergyIdentifiers.getHeatingOil());	
+					break;		
+				case CommodityIdentifiers.LEAN_HOGS:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(MeatIdentifiers.getLeanHogs());	
+					break;
+				case CommodityIdentifiers.LIVE_CATTLE:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(MeatIdentifiers.getLiveCattle());	
+					break;
+				case CommodityIdentifiers.LUMBER:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(SoftIdentifiers.getLumber());	
+					break;	
+				case CommodityIdentifiers.MILK_CLASS_III:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(MeatIdentifiers.getMilkClassIII());	
+					break;	
+				case CommodityIdentifiers.NATURAL_GAS:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(EnergyIdentifiers.getNaturalGas());	
+					break;	
+				case CommodityIdentifiers.OATS:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(GrainIdentifiers.getOats());	
+					break;	
+				case CommodityIdentifiers.ORANGE_JUICE:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(SoftIdentifiers.getOrangeJuice());	
+					break;	
+				case CommodityIdentifiers.PLATINUM:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(MetalIdentifiers.getPlatinum());	
+					break;	
+				case CommodityIdentifiers.RBOB_GAS:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(EnergyIdentifiers.getRBOBGas());	
+					break;	
+				case CommodityIdentifiers.SILVER:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(MetalIdentifiers.getSilver());	
+					break;	
+				case CommodityIdentifiers.SOYBEAN_MEAL:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(GrainIdentifiers.getSoybeanMeal());	
+					break;	
+				case CommodityIdentifiers.SOYBEAN_OIL:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(GrainIdentifiers.getSoybeanOil());	
+					break;	
+				case CommodityIdentifiers.SOYBEANS:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(GrainIdentifiers.getSoybeans());	
+					break;	
+				case CommodityIdentifiers.SUGAR:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(SoftIdentifiers.getSugar());	
+					break;	
+				case CommodityIdentifiers.WHEAT:
+					if (!first) sb.append("+");
+					else first = false;
+					sb.append(GrainIdentifiers.getWheat());	
+					break;
+				default:
+					break;
+			}
+		}
+		
+		if (!sb.toString().equals("") && sb != null) {
+			String add = BASE_URL + sb.toString() + TAGS;
+			return parseData(add);
+		}
+		
+		return null;
+	}
 	
 	public static List<CommodityData> getEnergyData() throws IOException {
 
@@ -76,28 +216,11 @@ public class CommoditiesDataProvider {
 			CommodityData newData = new CommodityData(st.nextToken(), st.nextToken(), st.nextToken(),
 					st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken(),	st.nextToken(), 
 					st.nextToken(), st.nextToken());
-			System.out.println(newData.toString());
+
 			data.add(newData);
 		}
 		
 		return data;
 	}
-	
-//	public static void getData() throws IOException {
-//		//String uri = "http://download.finance.yahoo.com/d/quotes.csv?s=SMH14.CBT&f=sl1c1d1";
-//		String uri = BASE_URL + EnergyIdentifiers.getAllEnergies() + TAGS;
-//		URL url = new URL(uri);
-//		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//		conn.setRequestMethod("GET");
-//		
-//		conn.connect();
-//		InputStream in = conn.getInputStream();
-//		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-//		String text = null;
-//		
-//		while ((text = reader.readLine()) != null)	System.out.println(text);
-//
-//		conn.disconnect();
-//	}
 	
 }
